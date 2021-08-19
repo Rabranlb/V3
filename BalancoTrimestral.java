b754fdfe3fa12b42965813ceeeae801b93bb172c
Raphael Leal <raphaelleal990@gmail.com>
2021-08-19 18:06:47 +0000

    Novas linhas de código

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,7 +1,11 @@
 public class BalancoTrimestral{ 
     public static void main(String args[]){
         int gastosJaneiro=15000;
-        int gastosFevereiro;
-        int gastosMarco;
+        int gastosFevereiro=23000;
+        int gastosMarco=17000;
+        int gastosTrimestral = gastosJaneiros+gastosFevereiro+gastosMarco;
+        int gastoMensal = gastosTrimestral/3;
+        System.out.println("O gasto trimestral foi de: " + gastosTrimestral);
+        System.out.println("O gasto mensal foi de: " + gastoMensal);
     }
 }
