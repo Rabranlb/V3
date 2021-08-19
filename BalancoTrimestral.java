1b316ff87c6fda88df46c9aa838ddcd74dd4273c
Raphael Leal <raphaelleal990@gmail.com>
2021-08-19 17:54:58 +0000

    Novas variáveis

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,3 +1,7 @@
 public class BalancoTrimestral{ 
-    
+    public static void main(String args[]){
+        int gastosJaneiro=15000;
+        int gastosFevereiro;
+        int gastosMarco;
+    }
 }
